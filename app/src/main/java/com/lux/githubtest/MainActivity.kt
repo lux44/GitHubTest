236ecc6d57df092f61eebe17f01e7d52943af962
lux44 <104046642+lux44@users.noreply.github.com>
2022-04-20 13:57:20 +0900

    Update MainActivity.kt

--- app/src/main/java/com/lux/githubtest/MainActivity.kt
+++ app/src/main/java/com/lux/githubtest/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.title="테스트"
         var a =10
     }
 }
